fedd27bb6c826cdc4043f96179ea99c63b99d334
younghun <gofhddl5910@naver.com>
2023-04-14 11:32:39 +0900

    v1

---
+++ src/main/java/com/kh/myapp/myappApplication.java
@@ -0,0 +1,13 @@
+package com.kh.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class myappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(myappApplication.class, args);
+	}
+
+}
